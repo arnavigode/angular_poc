c8cb22cff26d583a073b02bbb9ebe74eb7ad728a
arnavigode <arnavi.gode96@gmail.com>
2022-11-13 13:41:50 +0530

    header comp added

--- poc-angular/src/app/app.component.spec.ts
+++ poc-angular/src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixtuare.componentInstance;
     expect(app).toBeTruthy();
   });
 
